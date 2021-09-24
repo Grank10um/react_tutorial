0157e29a9e2d611a24fbcef40152d6c8992a8158
grank10um <754864@gmail.com>
2021-09-24 18:58:49 +0300

    пропсы с постами

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
+import PostItem from './components/PostItem';
+import "./styles/App.css"
 
 
 function App() {
   const [value, setValue] = useState('text in input');
 
-
-
-
   return (
     <div className="App">
-      <Counter/>
-       <ClassCounter/>
-
+      <PostItem post={{id: 1, title: 'JavaSc', body: 'description'}} />
     </div>
   );
-}
+};
 
 export default App;
