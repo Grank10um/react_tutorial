4c63639faf5bb9ee09e7e1773086979346143755
grank10um <754864@gmail.com>
2021-09-24 19:39:07 +0300

    пропсы с постами2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import "./styles/App.css"
 
 
 function App() {
-  const [value, setValue] = useState('text in input');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaSc', body: 'description'},
+    {id: 2, title: 'Java2Sc', body: 'desc543ription'},
+    {id: 3, title: 'Jav4aSc', body: 'descr534iption'},
+    {id: 4, title: 'Jav3aSc', body: 'descrertgiption'},
+
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'JasdfsvaSc', body: 'description'},
+    {id: 2, title: 'sdf', body: 'desc543ription'},
+    {id: 3, title: 'Jav4sfrewfsaSc', body: 'descr534iption'},
+    {id: 4, title: 'Javwerfdw3aSc', body: 'descrertgiption'},
+
+  ]);
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaSc', body: 'description'}} />
+      <PostList posts={posts} title="Postlist1"/>
+      <PostList posts={posts2} title="Postlistdsf1"/>    
     </div>
   );
 };
