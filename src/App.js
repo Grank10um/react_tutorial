78187ecc7a6227fe16b2222e0ae01f0f14932311
grank10um <754864@gmail.com>
2021-09-26 21:06:28 +0300

    пропс useref для обращения к любому дом элементу

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyInput from './components/UI/button/input/MyInput';
 import MyButton from './components/UI/button/MyButton';
 import "./styles/App.css"
 
@@ -16,14 +17,30 @@ function App() {
 
   ]);
 
+  const [title, setTitle] =useState('')
+  const bodyInputRef = useRef(); 
 
+const addNewPost = (e) => {
+  e.preventDefault()
+  console.log(title)
+  {/*console.log(bodyInputRef.current.value)  метод с хуком юзреф*/} 
+
+}
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post name"/>
-        <input type="text" placeholder="Post description"/>
-        <MyButton disabled>Create Post</MyButton>
+        <MyInput 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="post Name"
+        />
+       {/*} <input ref={bodyInputRef} type="text" /> обращение к любому дом елементу*/}
+
+
+        <MyInput type="text" placeholder="Post description"/>
+        <MyButton onClick={addNewPost} >Create Post</MyButton>
       </form>
       <PostList posts={posts} title="Postlist1"/>  
     </div>
